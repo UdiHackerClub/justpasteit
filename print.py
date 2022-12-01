75e4329fae698aabb7957f94f5dcb7e0aa4c2017
Uberlândia Hacker Club <114319518+UdiHackerClub@users.noreply.github.com>
2022-12-01 12:04:21 -0300

    update
    this update rename the print name

--- print.py
+++ print.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 from time import sleep
+import os
+import re
+import os
+
+_src = "/home/otavio/Desktop/Projects/juspasteit/"
+_ext = ".png"
 
 myfile = open("200.txt", "r")
 
 url = myfile.readline()
 
-site = url
 while url:
     url = myfile.readline() 
 
@@ -13,8 +18,12 @@ while url:
     driver.get(url)
     sleep(1)
 
-    driver.get_screenshot_as_file("screenshot.png")
+    driver.get_screenshot_as_file("printou.png")
     driver.quit()
-    print("end...")
+    print("renaming...")
+
+    for i,filename in enumerate(os.listdir(_src)):
+        if filename.endswith(_ext):
+            os.rename(filename, _src+'Sites-' + str(i).zfill(3)+_ext)
 
 myfile.close()
